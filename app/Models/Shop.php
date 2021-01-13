46c6f9b39d706dfa5c3cdf157cd68c5d65f625c6
roy-amelie <amelie.roy@media-io.com>
2021-01-13 15:22:13 +0100

    add shop relation

--- app/Models/Shop.php
+++ app/Models/Shop.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Shop extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'postal_code',
+        'city'];
+
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class);
+    }
 }
