40e62227c8dc615cda0586a960f65e54caa97304
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2021-01-13 16:12:55 +0100

    creation product form

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/createproduct', function () {
+    return view('products.create');
+});
 
 Route::resource('categories',CategoryController::class);
 
