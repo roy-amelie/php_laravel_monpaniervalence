dc06ed235205390964c2f8e6f7badbb7ac9c9205
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2021-01-14 10:56:36 +0100

    design front for shops in every categories

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -1,21 +1,27 @@
 @extends('layout')
 
 @section('main')
-<main>
-    <h2>
-        {{ $category -> name }}
-    </h2>
-    @foreach($category->shops as $shop)
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">{{ $shop->name }}</h5>
-            <p class="card-text">{{ $shop->address }}</p>
-            <a href="{{ url('shops/' . $shop->id)}}" class="btn btn-primary">Go somewhere</a>
+    <main>
+        <h2>
+            {{ $category -> name }}
+        </h2>
+        <div class="container-fluid">
+            <div class="row">
+                @foreach($category->shops as $shop)
+                    <div class="card col-sm-6 col-md-4 col-lg-3 category text-center" style="width: 18rem;">
+                        <img src="{{url('/img/logo.png')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $shop->name }}</h5>
+                            <p class="card-text">{{ $shop->address }}</p>
+                            <p class="card-text">{{ $shop->postal_code }}</p>
+                            <p class="card-text">{{ $shop->city }}</p>
+                            <a href="{{ url('shops/' . $shop->id)}}" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
         </div>
-    </div>
-    @endforeach
-</main>
+    </main>
 @endsection
 
 
