95021b1d652d377855f5d896755626a504db59c5
roy-amelie <amelie.roy@media-io.com>
2021-01-14 11:25:21 +0100

    add all products for shop

--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -12,8 +12,24 @@
                     <p class="card-text">{{ $shop->address }}</p>
                     <p class="card-text">{{ $shop->postal_code }}</p>
                     <p class="card-text">{{ $shop->city }}</p>
-                    <a href="{{ url('shops/' . $shop->id)}}" class="btn btn-primary">Go somewhere</a>
                 </div>
+            </div>
+        </div>
+
+         <div class="container-fluid">
+            <div class="row">
+                @foreach($shop->products as $product)
+                    <div class="card col-sm-6 col-md-4 col-lg-3 category text-center" style="width: 18rem;">
+                        <img src="{{url('/img/shop.jpeg')}}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->name }}</h5>
+                            <p class="card-text">{{ $product->address }}</p>
+                            <p class="card-text">{{ $product->postal_code }}</p>
+                            <p class="card-text">{{ $product->city }}</p>
+                            <a href="{{ url('products/' . $product->id)}}" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                @endforeach
             </div>
         </div>
     </main>
