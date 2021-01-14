97d24d3470c6b9c61bd7e0fe04863395829b66c0
JonathanMaran <jonathan.maran@le-campus-numerique.fr>
2021-01-14 11:05:33 +0100

    shop design front

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -9,7 +9,7 @@
             <div class="row">
                 @foreach($category->shops as $shop)
                     <div class="card col-sm-6 col-md-4 col-lg-3 category text-center" style="width: 18rem;">
-                        <img src="{{url('/img/logo.png')}}" class="card-img-top" alt="...">
+                        <img src="{{url('/img/shop.jpeg')}}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $shop->name }}</h5>
                             <p class="card-text">{{ $shop->address }}</p>
--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -2,6 +2,19 @@
 
 @section('main')
     <main>
-        <p>  Shop {{ $shop->name }}</p>
+        <h2> Shop {{ $shop->name }}</h2>
+        <div class="container-fluid">
+            <div class="row">
+
+                <img src="{{url('/img/shop.jpeg')}}" class="col-6" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $shop->name }}</h5>
+                    <p class="card-text">{{ $shop->address }}</p>
+                    <p class="card-text">{{ $shop->postal_code }}</p>
+                    <p class="card-text">{{ $shop->city }}</p>
+                    <a href="{{ url('shops/' . $shop->id)}}" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
     </main>
 @endsection
